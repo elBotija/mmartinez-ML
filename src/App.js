5717a73dfa515da578d4932ea4b697aff10fc845
murillo <murillo@localhost.localdomain>
2018-08-15 14:18:16 -0300

    product list

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ class App extends Component {
             query: '',
             search: [],
             item:[],
-            itemDescription:[]
+            itemDescription:[],
+            filters:[]
         }
 
         this.handleChange = this.handleChange.bind(this);
@@ -24,6 +25,7 @@ class App extends Component {
                 console.log('res')
                 console.log(response)
                 this.setState({search: response.data.results})
+                this.setState({filters: response.data.filters})
                 console.log('y la data?')
                 console.log(this.state.search)
             })
@@ -52,6 +54,26 @@ class App extends Component {
         })
     }
 
+    hasFreeShipping(show){
+        if(show){
+            return(
+                <div className="free-shipping">Envio Gratis</div>
+            )
+        }
+    }
+
+    showCategories(){
+        return this.state.filters.map(filter => {
+            if(filter.id == "category") {
+                return filter.values.map(value =>{
+                     return value.path_from_root.map(category => {
+                        return(<span>{category.name}</span>)
+                    })
+                })
+            }
+        })
+    }
+
 
   render() {
     return (
@@ -62,39 +84,55 @@ class App extends Component {
         </header> */}
 
 
-    <nav className="navbar navbar-expand-lg">
-        <div className="container col-md-10">
-            <a className="navbar-brand col-1" href="/">
-                Mercado Libre Argentina - Donde comprar y vender de todo
-            </a>
-            <form  onSubmit={this.handleClick} className="input-group col-11">
-                <input type="text" name="query" onChange={this.handleChange} value={this.state.query} className="form-control" placeholder="Nunca dejes de buscar" autocomplete="off"/>
-                <div className="input-group-append">
-                    <button className="btn" onClick={this.handleClick} type="button">Buscar</button>
+        <nav className="navbar navbar-expand-lg">
+            <div className="container col-md-10">
+                <a className="navbar-brand col-1" href="/">
+                    Mercado Libre Argentina - Donde comprar y vender de todo
+                </a>
+                <form  onSubmit={this.handleClick} className="input-group col-11">
+                    <input type="text" name="query" onChange={this.handleChange} value={this.state.query} className="form-control" placeholder="Nunca dejes de buscar" autocomplete="off"/>
+                    <div className="input-group-append">
+                        <button className="btn" onClick={this.handleClick} type="button">Buscar</button>
+                    </div>
+                </form>
+            </div>
+        </nav>
+        <div className="container">
+            <div className="row">
+                <div className="categories">
+                    {
+                        this.showCategories()
+                    }
                 </div>
-            </form>
+                <div className="list-items col-12">
+                    {
+                        this.state.search.map(search => {
+                            return (
+                                <div className="item row" key={search.id}>
+                                    <figure className="figure col-3">
+                                        <img src={search.thumbnail} alt="" className="img-fluid"/>
+                                    </figure>
+                                    <div className="info col-9">
+                                        <span className="price">
+                                            <a onClick={() => this.detailProduct(search.id)}>
+                                                ${search.price}
+                                            </a>
+                                            {this.hasFreeShipping(search.shipping.free_shipping)}
+                                        </span>
+                                        <span className="zone text-rigth">Capital</span>
+                                        <h2 className="title">
+                                            <a onClick={() => this.detailProduct(search.id)}>
+                                                {search.title}
+                                            </a>
+                                        </h2>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
         </div>
-    </nav>
-
-            {
-                this.state.search.map(search => {
-                    return (
-                        <div key={search.id}>
-
-                                <span>
-                                    <a onClick={() => this.detailProduct(search.id)}>
-                                        {search.title}
-                                    </a>
-                                </span>
-                                <span>
-                                    <a onClick={() => this.detailProduct(search.id)}>
-                                        {search.price}
-                                    </a>
-                                </span>
-                        </div>
-                    )
-                })
-            }
       </div>
     );
   }
